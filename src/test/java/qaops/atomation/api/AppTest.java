85055642660f8070689b96c630b26dd160cfc6ea
Jayme Gomes <jayme.r.g.n@hotmail.com>
2021-10-27 22:52:33 -0300

    'atualiza all'

---
+++ src/test/java/qaops/atomation/api/AppTest.java
@@ -0,0 +1,17 @@
+package qaops.atomation.api;
+
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
+
+public class AppTest {
+
+    @Test
+    public void testNormal() {
+        assertThat(1, is(1));
+    }
+
+
+}
