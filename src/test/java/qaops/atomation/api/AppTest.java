73f5e80ddc2e3dc29a8ecb36b2c4bcff6838f91b
Jayme Gomes <jayme.r.g.n@hotmail.com>
2021-10-28 09:41:56 -0300

    first test

--- src/test/java/qaops/atomation/api/AppTest.java
+++ src/test/java/qaops/atomation/api/AppTest.java
@@ -1,16 +1,30 @@
 package qaops.atomation.api;
 
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-
-
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.is;
 
 public class AppTest {
 
     @Test
-    public void testNormal() {
-        assertThat(1, is(1));
+    public void testGetUsersStatic() {
+        when().
+                get("https://reqres.in/api/users?page=2").
+        then().
+                statusCode(200)
+                .body("page", is(2))
+                .body("data[0].email", is("michael.lawson@reqres.in"));
+    }
+
+
+    @Test
+    public void testGetUsersCorrect(){
+        when().
+                get("https://reqres.in/api/users?page=2").
+        then().
+                statusCode(HttpStatus.SC_OK).
+                body("page", is(2));
     }
 
 
