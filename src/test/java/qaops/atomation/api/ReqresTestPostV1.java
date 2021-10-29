9bfe182d5427ae04739c389b35c7e884c74c7d86
Jayme Gomes <jayme.r.g.n@hotmail.com>
2021-10-29 11:05:14 -0300

    add post with json serializer

--- src/test/java/qaops/atomation/api/ReqresTestPostV1.java
+++ src/test/java/qaops/atomation/api/ReqresTestPostV1.java
@@ -1,26 +1,39 @@
 package qaops.atomation.api;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import qaops.atomation.api.domain.User;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.core.Is.is;
+
 public class ReqresTestPostV1 {
 
 
-
-        /*
-    @Test
-    public void testPostCreateUser(){
-        given().
-                contentType(ContentType.JSON).
-                body("{\n" +
-                        "    \"name\": \"jayme\",\n" +
-                        "    \"job\": \"trainee\"\n" +
-                        "}").
-                when().
-                    post("https://reqres.in/api/users").
-                then().
-                    statusCode(HttpStatus.SC_CREATED).
-                    body("name", is("jayme")).
-                    body("job", is("trainee"));
+    @BeforeAll
+    public static void setup(){
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        baseURI = "https://reqres.in";
+        basePath = "/api";
     }
 
-    */
+    @Test
+    public void postUserSuccess(){
 
+        User user = new User("Jayme", "Trainee");
+
+        given().
+                contentType(ContentType.JSON).
+                body(user).
+        when().
+                post("/users").
+        then().
+                statusCode(HttpStatus.SC_CREATED).
+                body("name", is("Jayme")).
+                body("job", is("Trainee"));
+    }
+    
 }
